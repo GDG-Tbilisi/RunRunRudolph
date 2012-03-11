55f0324a4a89a3867a89966c45d74113cc81faaf
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2012-03-12 00:39:15 +0400

    options menu added + reset feature added @author: gtkesh

--- gen/ge/gtug/R.java
+++ gen/ge/gtug/R.java
@@ -14,12 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int result=0x7f050000;
-        public static final int start=0x7f050001;
+        public static final int about=0x7f060000;
+        public static final int restart=0x7f060003;
+        public static final int result=0x7f060001;
+        public static final int start=0x7f060002;
     }
     public static final class layout {
-        public static final int game=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int about=0x7f030000;
+        public static final int game=0x7f030001;
+        public static final int main=0x7f030002;
+    }
+    public static final class menu {
+        public static final int menu=0x7f050000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/ge/gtug/Game.java
+++ src/ge/gtug/Game.java
@@ -1,7 +1,11 @@
 package ge.gtug;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -26,7 +30,7 @@ public class Game extends Activity
 
 	public boolean onTouchEvent(MotionEvent event) {
 		// TODO Auto-generated method stub
-		if(event.getAction()==1){
+		if(event.getAction()==0){
 		
 		result.setText("Number of steps made: " + counter);
 		counter++; 
@@ -36,6 +40,30 @@ public class Game extends Activity
   		return false;
 	}
 
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		super.onCreateOptionsMenu(menu);
+		MenuInflater koba = getMenuInflater();
+		koba.inflate(R.menu.menu, menu);
+		return true;
+		
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.about:
+			startActivity(new Intent(this, About.class));
+			return true;
+		case R.id.restart:
+			counter = 0;
+			result.setText("Number of steps made: " + counter);
+			
+		default:
+			return super.onOptionsItemSelected(item);
+
+		}
+	}
 
 //	public boolean onTouch(View v, MotionEvent event) {
 //		// TODO Auto-generated method stub
