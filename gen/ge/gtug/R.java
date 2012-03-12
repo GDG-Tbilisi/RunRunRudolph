54a704c7acb668b9e945239d6de3bc9120a0471c
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2012-03-12 22:28:33 +0400

    DrawableSwitches on onTouchEvent added (with some errors) @author:gtkesh

--- gen/ge/gtug/R.java
+++ gen/ge/gtug/R.java
@@ -11,18 +11,34 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_launcer=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int meeqvse=0x7f020002;
+        public static final int meore=0x7f020003;
+        public static final int meotxe=0x7f020004;
+        public static final int mesame=0x7f020005;
+        public static final int meshvide=0x7f020006;
+        public static final int mexute=0x7f020007;
+        public static final int pirveli=0x7f020008;
     }
     public static final class id {
         public static final int about=0x7f060000;
-        public static final int restart=0x7f060003;
+        public static final int meeqvse=0x7f060008;
+        public static final int meore=0x7f060004;
+        public static final int meotxe=0x7f060006;
+        public static final int mesame=0x7f060005;
+        public static final int meshvide=0x7f060009;
+        public static final int mexute=0x7f060007;
+        public static final int pirveli=0x7f060002;
+        public static final int restart=0x7f06000a;
         public static final int result=0x7f060001;
-        public static final int start=0x7f060002;
+        public static final int start=0x7f060003;
     }
     public static final class layout {
         public static final int about=0x7f030000;
         public static final int game=0x7f030001;
         public static final int main=0x7f030002;
+        public static final int men=0x7f030003;
     }
     public static final class menu {
         public static final int menu=0x7f050000;
--- src/ge/gtug/Game.java
+++ src/ge/gtug/Game.java
@@ -9,12 +9,14 @@ import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Game extends Activity 
 {
-	TextView result;
-
+	public TextView result;
+	public ImageView pirveli, meore, mesame, meotxe, mexute, meeqvse, meshvide;
+	int[] drawableIds = {R.drawable.meore, R.drawable.mesame, R.drawable.meotxe, R.drawable.mexute, R.drawable.meeqvse, R.drawable.meshvide};
 	int counter=0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,28 +25,45 @@ public class Game extends Activity
 		setContentView(R.layout.game);
 		result = (TextView) findViewById(R.id.result);
 	  	result.setText("Number of steps made: " + counter);  
+	  	pirveli = (ImageView) findViewById(R.drawable.pirveli);
+	  //	pirveli.setOnTouchListener((OnTouchListener) this);
+	  	
+	  	meore = (ImageView) findViewById(R.drawable.meore);
+	  	mesame = (ImageView) findViewById(R.drawable.mesame);
+	  	meotxe = (ImageView) findViewById(R.drawable.meotxe);
+	  	mexute = (ImageView) findViewById(R.drawable.mexute);
+	  	meeqvse = (ImageView) findViewById(R.drawable.meeqvse);
+	  	meshvide= (ImageView) findViewById(R.drawable.meshvide);
 	}
 
 	public boolean onTouchEvent(MotionEvent event) {
-		// TODO Auto-generated method stub
-		if(event.getAction()==0){
-		
-		result.setText("Number of steps made: " + counter);
-		counter++; 
-		}
-		
-		
-  		return false;
+		  if(event.getAction()==MotionEvent.ACTION_DOWN){
+		     result.setText("Number of steps made: " + counter);
+		     counter++;
+		     pirveli.setImageResource(drawableIds[ drawableIds.length % ++counter]);
+		  }
+		  return false;
+	
+//	public boolean onTouchEvent(MotionEvent event) {
+//	    // TODO Auto-generated method stub
+//	    if(event.getAction() == MotionEvent.ACTION_DOWN){
+//	        if (result != null);
+//	            result.setText("Number of steps made: " + counter);
+//	        counter++; 
+//
+//	        if (pirveli != null)
+//	            pirveli.setImageResource(R.drawable.meore);
+//	        return true;
+//	    }
+//	    return false;
 	}
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO Auto-generated method stub
 		super.onCreateOptionsMenu(menu);
 		MenuInflater koba = getMenuInflater();
 		koba.inflate(R.menu.menu, menu);
-		return true;
-		
+		return true;	
 	}
-	
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// TODO Auto-generated method stub
 		switch (item.getItemId()) {
