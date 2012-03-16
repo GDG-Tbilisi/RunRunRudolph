12e74feef9b023a825386c3b8f6d392438946841
Giorgi Tkeshelashvili <gtkesh@gmail.com>
2012-03-16 21:14:03 +0400

    Timer Added `

--- src/ge/gtug/Game.java
+++ src/ge/gtug/Game.java
@@ -1,8 +1,9 @@
-package ge.gtug;
+ package ge.gtug;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -11,20 +12,22 @@ import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Game extends Activity 
 {
-	public TextView result;
+	public TextView result, time;
 	public ImageView pirveli, meore, mesame, meotxe, mexute, meeqvse, meshvide;
 	int[] drawableIds = {R.drawable.pirveli,R.drawable.meore, R.drawable.mesame, R.drawable.meotxe, R.drawable.mexute, R.drawable.meeqvse, R.drawable.meshvide};
 	int counter=0;
+	private CountDownTimer timer;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game);
 		result = (TextView) findViewById(R.id.result);
-	  	result.setText("Number of steps made: " + counter);  
+	  	result.setText("Steps made: " + counter);  
 	  	pirveli = (ImageView) findViewById(R.id.pirveli);
 	  	meore = (ImageView) findViewById(R.id.meore);
 	  	mesame = (ImageView) findViewById(R.id.mesame);
@@ -32,13 +35,39 @@ public class Game extends Activity
 	  	mexute = (ImageView) findViewById(R.id.mexute);
 	  	meeqvse = (ImageView) findViewById(R.id.meeqvse);
 	  	meshvide= (ImageView) findViewById(R.id.meshvide);
+	    time = (TextView) findViewById(R.id.time);
+	    final MyCounter timer = new MyCounter(20000,1000);
+	    timer.start();
 	}
 
+	public class MyCounter extends CountDownTimer{
+		 
+        public MyCounter(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+ 
+        @Override
+        public void onFinish() {
+//            System.out.println("Timer Completed.");
+            time.setText("");
+            Toast.makeText(getBaseContext(), "Time is up! Your score is: "+ counter, 3000).show();
+            Intent resultIntent = new Intent ("ge.gtug.RESULTS");
+            startActivity(resultIntent);
+        }
+ 
+        @Override
+        public void onTick(long millisUntilFinished) {
+            time.setText((millisUntilFinished/1000)+"");
+            System.out.println("Timer  : " + (millisUntilFinished/1000));
+        }
+    }
+	
+    
 	public boolean onTouchEvent(MotionEvent event) {
 		  if(event.getAction()==MotionEvent.ACTION_DOWN){
-		     result.setText("Number of steps made: " + counter);
-		     counter++;
-		    meshvide.setImageResource(drawableIds[counter % drawableIds.length]);
+		     result.setText("Steps made: " + counter);
+		     counter++;    
+		     meshvide.setImageResource(drawableIds[counter % drawableIds.length]);
 		  }
 		  return false;
 	}
@@ -57,8 +86,7 @@ public class Game extends Activity
 			return true;
 		case R.id.restart:
 			counter = 0;
-			result.setText("Number of steps made: " + counter);
-			
+			result.setText("Number of steps made: " + counter);		
 		default:
 			return super.onOptionsItemSelected(item);
 		}
