4f5d9d6fee4215c3b5ff465976746486eaa5a371
Demur Nodia <demuri111@gmail.com>
2012-03-30 01:39:51 +0400

    add entry (not used yes)

---
+++ src/ge/gtug/entry/PlayersEntry.java
@@ -0,0 +1,22 @@
+package ge.gtug.entry;
+
+public class PlayersEntry {
+	String name;
+	String point;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPoint() {
+		return point;
+	}
+	public void setPoint(String point) {
+		this.point = point;
+	}
+	public PlayersEntry(String name, String point){
+		this.name = name;
+		this.point = point;
+	}
+}
