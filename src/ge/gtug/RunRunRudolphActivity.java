f9c7460e121fbc25c6f0404f1ef573689db725a2
Demur Nodia <demuri111@gmail.com>
2012-04-26 08:59:28 +0400

    Run button visibility fixed

--- src/ge/gtug/RunRunRudolphActivity.java
+++ src/ge/gtug/RunRunRudolphActivity.java
@@ -60,14 +60,15 @@ public class RunRunRudolphActivity extends Activity {
 				
 				public void onClick(View v) {
 					// TODO Auto-generated method stub
-					start.setVisibility(View.VISIBLE);
 					String name  = edName.getText().toString().trim();
 					if(name!=null && !name.equals("")){	//checkPlayer(name);
 						int score = checkPlayer(name);
 						if(score>0){
+							start.setVisibility(View.VISIBLE);
 							txtNewPlayer.setText("Hello [ " + name + " ] your best score is [ " + score + " ] \n Click Start and try again");
 						}else{
-							txtNewPlayer.setText("Hey [ " + name + " ] you are a new Player \n best score on this device is " + score + " \n click Run and start!");
+							start.setVisibility(View.VISIBLE);
+							txtNewPlayer.setText("Hey [ " + name + " ] you are a new Player \n best score on this device is [ " + score + " ] \n click Run and start!");
 						}
 					}else{
 						txtNewPlayer.setText(" Please Type your Name ");
